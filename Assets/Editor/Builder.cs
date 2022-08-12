3a3751e204252258b8ec946e5961405ac253af84
Kirurobo <kirurobo@gmail.com>
2022-08-12 20:00:51 +0900

    namespaceを追加

--- Assets/Editor/Builder.cs
+++ Assets/Editor/Builder.cs
@@ -4,42 +4,47 @@ using UnityEditor;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 
-public static class Builder
+namespace Kirurobo
 {
-    [MenuItem("Build/OSX")]
-    public static void BuildOSX()
+    public static class Builder
     {
-        Build("macOS", BuildTarget.StandaloneOSX);
-    }
-    
-    [MenuItem("Build/Windows64")]
-    public static void BuildWindows64()
-    {
-        Build("Win64", BuildTarget.StandaloneWindows64, ".exe");
-    }
-    
-    public static void Build(string label, BuildTarget target, string binaryExtension = "")
-    {
-        var appName = PlayerSettings.productName;
-        
-        var projectDir = Directory.GetCurrentDirectory();
-        var buildDir = Path.Join(projectDir, "Build", label);    
-        var outputPath = Path.Join(buildDir, appName + binaryExtension);
-        
-        var buildPlayerOptions = new BuildPlayerOptions();
-        buildPlayerOptions.scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled).Select(scene => scene.path).ToArray();
-        buildPlayerOptions.locationPathName = outputPath;
-        buildPlayerOptions.target = target;
-        buildPlayerOptions.options = BuildOptions.None;
-
-        var report = BuildPipeline.BuildPlayer(buildPlayerOptions);
-
-        if (report.summary.result == BuildResult.Succeeded)
+        [MenuItem("Build/OSX")]
+        public static void BuildOSX()
         {
-            Debug.Log($"Build succeeded: {report.summary.totalSize} bytes");
-        } else if (report.summary.result == BuildResult.Failed)
+            Build("macOS", BuildTarget.StandaloneOSX);
+        }
+
+        [MenuItem("Build/Windows64")]
+        public static void BuildWindows64()
         {
-            Debug.Log($"Build failed: {report.summary.totalErrors} errors");
+            Build("Win64", BuildTarget.StandaloneWindows64, ".exe");
+        }
+
+        public static void Build(string label, BuildTarget target, string binaryExtension = "")
+        {
+            var appName = PlayerSettings.productName;
+
+            var projectDir = Directory.GetCurrentDirectory();
+            var buildDir = Path.Join(projectDir, "Build", label);
+            var outputPath = Path.Join(buildDir, appName + binaryExtension);
+
+            var buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = EditorBuildSettings.scenes.Where(scene => scene.enabled)
+                .Select(scene => scene.path).ToArray();
+            buildPlayerOptions.locationPathName = outputPath;
+            buildPlayerOptions.target = target;
+            buildPlayerOptions.options = BuildOptions.None;
+
+            var report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+            if (report.summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log($"Build succeeded: {report.summary.totalSize} bytes");
+            }
+            else if (report.summary.result == BuildResult.Failed)
+            {
+                Debug.Log($"Build failed: {report.summary.totalErrors} errors");
+            }
         }
     }
 }
